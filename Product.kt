f74b2470c2f3176bc957a526ce53104517746987
Raíssa B Oliveira <rbpo@discente.ifpe.edu.br>
2025-10-24 11:03:45 -0300

    Create Product.kt

---
+++ Product.kt
@@ -0,0 +1,12 @@
+package com.example.novoprojeto.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
+data class Product(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val price: Double
+)
